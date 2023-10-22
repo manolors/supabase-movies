9468003a1853b0939aa70b6e6b84f329e694e6aa
manolors <manolo.rs2005@gmail.com>
2023-10-22 13:29:46 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient("https://ohlyjgjofqzytmtwwzna.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9obHlqZ2pvZnF6eXRtdHd3em5hIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NTMxNTc3NSwiZXhwIjoyMDEwODkxNzc1fQ.xfXMM6hYtwMSWhnpW21m4GlEKijfAK0vub6izvUxPWk");
