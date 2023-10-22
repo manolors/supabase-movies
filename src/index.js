40aeeba396251b14455d7a18ce928469b603e7de
manolors <manolo.rs2005@gmail.com>
2023-10-22 13:48:10 +0100

    remove env vars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient("https://ohlyjgjofqzytmtwwzna.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9obHlqZ2pvZnF6eXRtdHd3em5hIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NTMxNTc3NSwiZXhwIjoyMDEwODkxNzc1fQ.xfXMM6hYtwMSWhnpW21m4GlEKijfAK0vub6izvUxPWk");
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || false;
+const SUPABASE_SECRET = import.meta.env.VITE_SUPABASE_SECRET || false;
+const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET);
+
+console.log(supabase);
